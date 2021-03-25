f3006583dc164f2e2366f40a26ba86289079117e
krircc <krircc@qq.com>
2021-03-25 19:42:15 +0800

    user

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,7 +1,8 @@
-use sqlx::postgres::PgPool;
+use sqlx::{Row, postgres::{PgPool,PgRow}};
 use ntex_files as fs;
 use ntex::web::{types, HttpResponse, Error};
 use crate::model::User;
+use futures::stream::TryStreamExt;
 
 pub async fn index() -> Result<fs::NamedFile, Error> {
     Ok(fs::NamedFile::open("public/index.html")?)
@@ -10,8 +11,12 @@ pub async fn index() -> Result<fs::NamedFile, Error> {
 
 pub async fn user( dbpool: types::Data<sqlx::PgPool> ) -> Result<HttpResponse, Error> {
 
-    let auser = sqlx::query("SELECT * FROM users").fetch(&**dbpool);
+    let auser = sqlx::query("SELECT * FROM users")
+    .map(|row: PgRow| {
+        let one: &str = row.try_get(1).unwrap();
+        println!("-------{:?}--------------", one);
+        // map the row into a user-defined domain type
+    }).fetch_all(&**dbpool).await.unwrap();
 
-    // println!("-------{:?}--------------", &auser.email);
     Ok(HttpResponse::Ok().json(&0))
 }
